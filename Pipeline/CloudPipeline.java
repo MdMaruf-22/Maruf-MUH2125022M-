44b06aea19730b743233ba4c62b296acef29e7b8
Maruf <marufislam74208@gmail.com>
2024-05-26 10:43:48 +0600

    Tried to run them to check

--- Pipeline/CloudPipeline.java
+++ Pipeline/CloudPipeline.java
@@ -1,6 +1,25 @@
 package Pipeline;
 
 public class CloudPipeline extends DataPipeline{
-    
-
+    @Override
+    public boolean connect(String url){
+        boolean connect = false;
+        System.out.println("Connecting cloud....");
+        connect = true;
+        return connect;
+    }
+    @Override
+    public boolean transformData(){
+        boolean transform = false;
+        System.out.println("Transforming Data....");
+        transform = true;
+        return transform;
+    }
+    @Override
+    public boolean storeData(){
+        boolean store = false;
+        System.out.println("Storing Data....");
+        store = true;
+        return store;
+    }
 }
